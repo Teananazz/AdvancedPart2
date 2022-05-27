e68457eddfb6ef583d730978a707eede91124082
galsh26 <galsh26@gmail.com>
2022-05-27 11:05:00 +0300

    fixed little problem

--- Controllers/RatingsController.cs
+++ Controllers/RatingsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Advanced.Data;
 using Advanced.Models;
+using Advanced.Services;
 
 namespace Advanced.Controllers
 {
